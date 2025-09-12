517b67a7f8ae7dbf7fd9b5d7d9e5e031324d8af1
mukul-pixel <mukul8756@gmail.com>
2025-09-12 20:26:04 +0530

    changed the notebook viewer platform

--- my-portfolio/src/Components/Apprecommendation.js
+++ my-portfolio/src/Components/Apprecommendation.js
@@ -5,7 +5,7 @@ export const Apprecommendation = () => {
     <>
     <h2 className='text-align-center'>Apprecommendation</h2>
     <iframe
-  src="https://mukul-pixel.github.io/app_recommendation_analysis/Basics.html"
+  src="https://colab.research.google.com/github/mukul-pixel/app_recommendation_analysis/blob/main/Basics.ipynb"
   width="100%"
   height="600"
   style={{ border: "none" }}
