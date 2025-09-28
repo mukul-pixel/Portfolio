a9e313b275597fa991da255bc1821c5d78ec08c7
mukul-pixel <mukul8756@gmail.com>
2025-09-28 12:50:54 +0530

    added aarrr funnel in pcs-1

--- my-portfolio/src/Components/Casestudynavi.js
+++ my-portfolio/src/Components/Casestudynavi.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import naviHeroImage from '../assets/Navi Hero image.png';
+import aarrrFunnel from '../assets/aarrr funnel.png';
 
 export const Casestudynavi = () => {
   return (
@@ -33,6 +34,45 @@ export const Casestudynavi = () => {
                     className='navi-hero-img w-auto'
                     src={naviHeroImage}
                     alt='navi_hero_img'
+                    style={{ height: "500px", objectFit: "cover" }}
+                    />
+            </div>
+        </div>
+        <div className='row aarrr-funnel-wrapper'>
+            <h4 className='text-decoration-underline my-4'>AARRR Funnel</h4>
+            <div className='aarrr-funnel-text col-md-6 col-12 fs-5'>
+                <h5 className='acquisition-heading'>Acquisition</h5>
+                <ul>
+                    <li>Performance marketing (Google, Facebook, YouTube ads).</li>
+                    <li>Word of mouth: “Instant loans in 5 mins.”</li>
+                </ul>
+                <h5>Activation</h5>
+                <ul>
+                    <li>Smooth KYC onboarding in {'<'}10 mins.</li>
+                    <li>Personalized loan/insurance offers.</li>
+                </ul>
+                <h5>Retention</h5>
+                <ul>
+                    <li>EMI reminders, push notifications.</li>
+                    <li>Simplified investment journeys.</li>
+                    <li>Gamification - Navi Coins</li>
+                </ul>
+                <h5>Revenue</h5>
+                <ul>
+                    <li>Interest Income from personal loans.</li>
+                    <li>Premiums & underwriting margins on health insurance.</li>
+                    <li>Expense ratios on mutual funds</li>
+                </ul>
+                <h5>Referral</h5>
+                <ul>
+                    <li>Referral bonuses (₹100–500)</li>
+                </ul>
+            </div>
+            <div className='aarrr-funnel-img col-md-6 col-12 py-4'>
+                <img 
+                    className='aarrr-funnel-img-container w-auto'
+                    src={aarrrFunnel}
+                    alt='aarrr_funnel_img'
                     style={{ height: "500px", objectFit: "cover" }}
                     />
             </div>
